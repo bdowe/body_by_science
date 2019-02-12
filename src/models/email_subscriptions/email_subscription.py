f63c9daceda370f10e6f8ccc1178cd27204caacb
Brian Dowe <briandowe@gmail.com>
2019-02-11 19:01:01 -0800

    Add backend for email subscription feature

---
+++ src/models/email_subscriptions/email_subscription.py
@@ -0,0 +1,38 @@
+from bson.objectid import ObjectId
+from flask import session
+from src.database import Database
+from src.utils import Utils
+import datetime
+
+from src.models.users.user import User
+
+
+class EmailSubscription(object):
+    @classmethod
+    def new(cls, email):
+        if email:
+            subscription = cls.getByEmail(email)
+            valid = Utils.email_is_valid(email)
+            if not subscription and valid:
+                Database.insert('email_subscriptions', {'email': email, 'timestamp': datetime.datetime.now()})
+                return True
+        return False
+
+    @staticmethod
+    def getByEmail(email):
+        if email:
+            subscription = Database.find_one('email_subscriptions', {'email': email})
+            if subscription:
+                return subscription
+        return None
+
+    @staticmethod
+    def getAll():
+        return Database.find('email_subscriptions', {})
+
+    @staticmethod
+    def delete(email):
+        if email:
+            Database.remove('email_subscriptions', {'email': email})
+            return True
+        return False
---
+++ src/models/email_subscriptions/views.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, request, session, render_template, jsonify, app
+
+from src.models.email_subscriptions.email_subscription import EmailSubscription
+
+email_subscription_blueprint = Blueprint('email_subscriptions', __name__)
+
+@email_subscription_blueprint.route('/subscribe/', methods=['POST'])
+def subscribe():
+    email = request.form['email']
+    status = "No Good"
+    message = "There was an error adding your email to our subscription list."
+    try:
+        subscribed = EmailSubscription.new(email)
+        if subscribed:
+            status = "OK"
+            message = "Success! Your email has been added to our subscription list!"
+        return jsonify({"status": status, "message": message, "email": email})
+
+    except Exception as e:
+        return "There was an error processing your subscription request."
