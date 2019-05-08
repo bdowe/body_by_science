20ed74beb9ac729d48f15aba78a3c97ba272da28
Brian Dowe <briandowe@gmail.com>
2019-05-07 21:40:09 -0700

    Add client and server side validation for registering and logging in

--- src/models/users/views.py
+++ src/models/users/views.py
@@ -5,7 +5,7 @@ from src.models.posts.post import Post
 import src.models.users.decorators as user_decorators
 from werkzeug.utils import redirect
 from src.app import app
-
+from src.utils import Utils
 
 user_blueprint = Blueprint('users', __name__)
 
@@ -35,6 +35,19 @@ def loginUser():
     password = request.form['password']
     status = "No Good"
     message = "Error - information provided does not match any account"
+
+    if not email or not password:
+        return 'Email and password are required to register'
+
+    if not Utils.email_is_valid(email):
+        return 'Email format is not valid'
+
+    if len(password) < 6:
+        return 'Password must be at least 8 characters'
+
+    if email not in app.config['ADMINS']:
+        return 'This email does not have admin priviliges'
+
     try:
         valid = User.isLoginValid(email, password)
         if valid:
@@ -53,8 +66,19 @@ def registerUser():
     password = request.form['password']
     status = "No Good"
     message = "An account with that email already exists"
+
+    if not email or not password:
+        return 'Email and password are required to register'
+
+    if not Utils.email_is_valid(email):
+        return 'Email format is not valid'
+
+    if len(password) < 6:
+        return 'Password must be at least 8 characters'
+
     if email not in app.config['ADMINS']:
         return 'This email does not have admin priviliges'
+
     try:
         registered = User.register(email, password)
         if registered:
