8ec43c8a7c48d97cc3e200f8ce62b5eab38cddf3
Brian Dowe <briandowe@gmail.com>
2019-06-18 11:34:50 -0700

    Deployment modifications

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ sys.path.insert(0, '/var/www/html/body-by-science/')
 from src.app import app
 
 if __name__ == '__main__':
-    app.run(port=4996, debug=True) # option to specify the port
+    app.run(debug=True) # option to specify the port
